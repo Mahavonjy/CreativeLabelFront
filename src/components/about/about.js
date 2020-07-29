7460cf98607185a343ac3444af21db3194297d2c
ramoRJ96 <moiseraidjy@gmail.com>
2020-07-29 03:24:07 +0300

    update about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,11 +10,28 @@ import landing from '../../assets/images/screenshots/landing.jpg';
 import photography from '../../assets/images/screenshots/photography.jpg';
 import shop from '../../assets/images/screenshots/shop.jpg';
 import one_page from '../../assets/images/screenshots/one_page.jpg';
+import {useSelector} from "react-redux";
 
 
 function About() {
 
+    const lightModeOn = useSelector(state => state.Home.lightModeOn);
     const isMounted = useRef(false);
+
+    const generateSection = (image, title) => {
+        return (
+            <div className="col-md-4 col-sm-6 col-xs-12">
+                <a className="content-box"
+                   href="">
+                    <div className={lightModeOn ? "content-box-image theme-light" : "content-box-image theme-dark"}><img
+                        src={image}
+                        alt="Main Demo"/></div>
+                    <h3 className={lightModeOn ? "content-box-title font-serif text-black " :
+                        "content-box-title font-serif text-white"}>{title}</h3>
+                </a>
+            </div>
+        )
+    };
 
     useEffect(() => {
 
@@ -32,7 +49,7 @@ function About() {
                             <article className="post-553 page type-page status-publish ast-article-single" id="post-553"
                                      itemType="https://schema.org/CreativeWork" itemScope="itemscope">
                                 <header className="text-center m-5">
-                                    <h1 className="text-red">A Propos de nous</h1>
+                                    <h1 className="font-serif text-red">A Propos de nous</h1>
                                 </header>
                                 {/* .entry-header */}
                                 <div className="entry-content clear" itemProp="text">
@@ -40,86 +57,19 @@ function About() {
                                          className="elementor elementor-553" data-elementor-settings="[]">
                                         <div className="elementor-inner">
                                             <div className="elementor-section-wrap">
+                                                {/*section image*/}
                                                 <section className="module-medium" id="demos">
                                                     <div className="container">
                                                         <div className="row multi-columns-row">
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box"
-                                                                   href="index_mp_fullscreen_video_background.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={main_demo}
-                                                                        alt="Main Demo"/></div>
-                                                                    <h3 className="content-box-title font-serif">Main
-                                                                        Demo</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box" href="index_agency.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={agency}
-                                                                        alt="Agency"/></div>
-                                                                    <h3 className="content-box-title font-serif">Agency</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box" href="index_portfolio.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={portfolio}
-                                                                        alt="Portfolio"/></div>
-                                                                    <h3 className="content-box-title font-serif">Portfolio</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box" href="index_restaurant.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={restaurant}
-                                                                        alt="Restaurant"/></div>
-                                                                    <h3 className="content-box-title font-serif">Restaurant</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box" href="index_finance.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={finance}
-                                                                        alt="Finance"/></div>
-                                                                    <h3 className="content-box-title font-serif">Finance</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box" href="index_landing.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={landing}
-                                                                        alt="Landing"/></div>
-                                                                    <h3 className="content-box-title font-serif">Landing</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box"
-                                                                   href="index_photography.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={photography}
-                                                                        alt="Photography"/></div>
-                                                                    <h3 className="content-box-title font-serif">Photography</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box" href="index_shop.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={shop}
-                                                                        alt="Shop"/></div>
-                                                                    <h3 className="content-box-title font-serif">Shop</h3>
-                                                                </a>
-                                                            </div>
-                                                            <div className="col-md-4 col-sm-6 col-xs-12">
-                                                                <a className="content-box"
-                                                                   href="index_op_fullscreen_gradient_overlay.html">
-                                                                    <div className="content-box-image"><img
-                                                                        src={one_page}
-                                                                        alt="One Page"/></div>
-                                                                    <h3 className="content-box-title font-serif">One
-                                                                        Page</h3>
-                                                                </a>
-                                                            </div>
+                                                            {generateSection(main_demo, 'Main demo')}
+                                                            {generateSection(agency, 'Agency')}
+                                                            {generateSection(portfolio, 'Portfolio')}
+                                                            {generateSection(restaurant, 'Restaurant')}
+                                                            {generateSection(finance, 'Finance')}
+                                                            {generateSection(landing, 'Landing')}
+                                                            {generateSection(photography, 'Photography')}
+                                                            {generateSection(shop, 'Shop')}
+                                                            {generateSection(one_page, 'One page')}
                                                         </div>
                                                     </div>
                                                 </section>
